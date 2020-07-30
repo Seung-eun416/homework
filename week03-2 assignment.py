e20fd59e49e7a7ee92fe52105741ef236362b717
Seung-eun416 <67549842+Seung-eun416@users.noreply.github.com>
2020-07-30 15:58:41 +0900

    3주차 짭짤한맛 숙제입니다.

---
+++ week03-2 assignment.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.homework
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+for tr in trs:
+    rank = tr.select_one('td.number').text[0:2].strip()
+    title = tr.select_one('td.info > a.title').text.strip()
+    artist = tr.select_one('td.info > a.artist').text
+    db.info.insert_one({'rank':rank, 'title':title, 'artist':artist})
